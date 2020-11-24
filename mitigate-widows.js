3e219f5cc1a910926008bb91621bce5e45c24ca6
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2020-11-24 01:09:56 -0500

    Update function and add comments for clarity

--- mitigate-widows.js
+++ mitigate-widows.js
@@ -1,33 +1,43 @@
 /**
  * Mitigate widows at the end of a string, replacing spaces with &nbsp;
  * @param {string} string - The string to modify
- * @param {string} [unbreakLimit = 12] - Unbroken part won't exceed this length
- * @returns {string} - String with final spaces up to unbreakLimit replaced
+ * @param {string} [unbreakLimit = 12] - Unbreakable end part won't exceed this length
+ * @returns {string} - String with unbreakable end part
  */
 function mitigateWidows(string, unbreakLimit = 12) {
   const parts = string.split(" ");
+  //  Reversing the parts makes them easier to work with,
+  //  since we're starting from the end of the string
   const reversedParts = parts.reduce((acc, part) => {
     acc.unshift(part);
     return acc;
   }, []);
-  const { breakGroup, unbreakGroup } = reversedParts.reduce(
-    (acc, part) => {
-      const { breakGroup, unbreakGroup } = acc;
-      const newUnbreakLen = [part].concat(unbreakGroup).join(" ").length;
-      const limitExceeded = breakGroup.length || newUnbreakLen > unbreakLimit;
-      if (limitExceeded) {
-        breakGroup.unshift(part);
-      } else {
-        unbreakGroup.unshift(part);
-      }
-      return acc;
-    },
-    { breakGroup: [], unbreakGroup: [] }
-  );
-  const breakString = breakGroup.join(" ");
-  const unbreakString = unbreakGroup.join("&nbsp;");
-  const bothNonEmpty = breakString && unbreakString;
-  return [breakString, unbreakString].join(bothNonEmpty ? " " : "");
+  //  Set up arrays for the breakable parts (at the start of the string)
+  //  and the unbreakable parts (at the end of the string)
+  let breakableParts = [];
+  let unbreakableParts = [];
+  reversedParts.forEach((part) => {
+    //  nextLength is how long the unbreakableParts would be
+    //  if we were to add this part
+    const nextLength = [part].concat(unbreakableParts).join(" ").length;
+    //  the unbreakLimit has been exceeded if nextLength is too long,
+    //  or if breakableParts already has items (ie we exceeded the limit earlier)
+    const limitExceeded = nextLength > unbreakLimit || breakableParts.length;
+    //  We're already working through items in reverse order;
+    //  we use `unshift` to add to our "parts" arrays
+    //  so they're built up back in their original order
+    if (limitExceeded) {
+      breakableParts.unshift(part);
+    } else {
+      unbreakableParts.unshift(part);
+    }
+  });
+  //  Assemble the breakable and unbreakable parts into strings
+  const breakString = breakableParts.join(" ");
+  const unbreakString = unbreakableParts.join("&nbsp;");
+  //  It's possible that either breakString and unbreakString are empty;
+  //  we need to filter out empty strings before joining to avoid extra spaces
+  return [breakString, unbreakString].filter((s) => s !== "").join(" ");
 }
 
 module.exports = mitigateWidows;
